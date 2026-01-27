2d358fcb2c38df1f5a68eddc8047073e3dafb980
Wenderson Santos <125162176+w-enderson@users.noreply.github.com>
2026-01-27 15:40:02 -0300

    Improve model evaluation and outlier detection
    Refactor model evaluation and outlier handling in R script.

--- modelagem.R
+++ modelagem.R
@@ -1,5 +1,3 @@
-
-
 # seed
 set.seed(28)
 
@@ -29,10 +27,10 @@ modelo1 <- train(formula1,
 modelo1_ <- glm(formula1, data = treino, family = "binomial")
 
 # Ajuste do modelo
-summary(modelo1_)
+summary(modelo1_) ## AIC : 183.26
 
 # Estatísticas do cross validation
-print(modelo1)
+print(modelo1) ## Acc : .8565
 
 
 # Teste de Razão de Verossimilhança
@@ -63,7 +61,7 @@ anova(modelo1_, modelo_reduzido_, test = "Chisq")
 # Teste de Razão de Verossimilhança para o modelo reduzido
 Anova(modelo_reduzido_, type = "III", test = "LR")
 
-## Todas as variáveis são importantes e trazem novas informações que ajudam na classificação
+## Todas as variáveis são importantes e trazem informações importantes
 
 set.seed(28)
 modelo2 <- train( condition ~  sex + cp +
@@ -73,30 +71,17 @@ modelo2 <- train( condition ~  sex + cp +
                   family = "binomial",
                   trControl = ctrl)
 
-resultados <- resamples(list(modelo_14_variaveis = modelo1, modelo_6_variaveis = modelo2))
-metricas_folds <- resultados$values
 
-summary(resultados)
-dotplot(resultados)
-
-## É possível observar que o modelo com 6 variáveis possui um poder de discriminação
-## maior, em relação ao modelo com 14 variáveis (AUC, prAUC)
-## O modelo de 6 variáveis também é mais estável
+# IC baseado em Perfil de Verossimilhança 
+ic_perfil <- confint(modelo_sem, level = 0.95)
+print(ic_perfil)
+## Vemos que algumas variáveis possuem ICs com grande amplitude;
+## Essas variáveis são justamente as variáveis indicadoras com pouca
+## representatividade no dataset;
 
 
 
-lista_de_modelos <- list(modelo_14_var = modelo1, modelo_6_var = modelo2)
 
-# Chame a nova função
-analise_otimizada <- analisar_modelos_otimizados(lista_de_modelos)
-
-# Veja os resultados agora com os thresholds aplicados!
-View(analise_otimizada$tabela)
-print(analise_otimizada$grafico)
-
-
-## O modelo reduzido é mais preciso (intervalo de confiança com menor amplitude), em relação ao modelo 
-## completo; Pelo princípio da parcimônia, optamos pelo uso do modelo reduzido
 
 
 
@@ -105,41 +90,97 @@ print(analise_otimizada$grafico)
 residuos_deviance <- residuals(modelo_reduzido_, type = "deviance")
 residuos_pearson <- residuals(modelo_reduzido_, type = "pearson")
 
+
 # Criar o QQ-Plot
 qqnorm(residuos_deviance, main = "QQ-Plot dos Resíduos Deviance (Modelo Reduzido)")
 qqline(residuos_deviance, col = "red", lwd = 2)
 
 
-plot(residuos_deviance)
-plot(residuos_pearson)
-
-
 plot(modelo_reduzido_)
-
-## É possível observar que há alguns candidatos a outliers (observações com maiores deviances)
-
-## Para verificar o impacto dessas observações, treinaremos um modelo sem essas observações para
-## verficar se eles impactam significativamente os coeficientes da regrressão
+plot_deviance_residuals(modelo_reduzido_, "Modelo 6 Variáveis")
 
 
+## nesses gráficos, é possível ver que há alguns pontos discrepantes em que o modelo
+## tem dificuldade de classificar
+nomes_outliers <- names(sort(abs(residuos_pearson), decreasing = TRUE)[1:5])
 
-# Identificar quais índices têm deviance absoluta maior que 3
-outliers_index <- which(abs(residuos_deviance) > 2.5)
+treino_sem_outliers <- treino[!(rownames(treino) %in% nomes_outliers), ]
+modelo_sem <- glm(condition ~ sex + cp + exang + oldpeak + slope + ca + thal,
+                  data = treino_sem_outliers, family = "binomial")
 
-# Ver quantos são e quais são
-print(outliers_index)
-
-# Se quiser ver esses dados na tabela original
-casos_estranhos <- treino[outliers_index, ]
-View(casos_estranhos)
+coef_comp = cbind(Com_Outliers = coef(modelo_reduzido_),
+                  Sem_Outliers = coef(modelo_sem))
+print(coef_comp)
 
 
+
+
+
+
+
+resultados <- resamples(list(modelo_14_variaveis = modelo1, modelo_6_variaveis = modelo2))
+metricas_folds <- resultados$values
+
+
+# Definindo melhor threshold para ambos os modelos
+
+preds_cv1 <- modelo1$pred
+roc_cv1 <- roc(response = preds_cv1$obs, 
+              predictor = preds_cv1$Doente,
+              levels = c("Saudavel", "Doente"))
+melhor_threshold1 <- coords(roc_cv1, "best", ret = "threshold")[[1]]
+# 0.4594418 
+
+preds_cv2 <- modelo2$pred
+roc_cv2 <- roc(response = preds_cv2$obs, 
+              predictor = preds_cv2$Doente,
+              levels = c("Saudavel", "Doente"))
+melhor_threshold2 <- coords(roc_cv2, "best", ret = "threshold")[[1]]
+# 0.3934961 
+
+
+# Aplicando para o Modelo 1 e Modelo 2
+metricas_m1 <- calcular_metricas_custom(modelo1$pred, melhor_threshold1) %>%
+  mutate(Modelo = "Modelo_14_var")
+
+metricas_m2 <- calcular_metricas_custom(modelo2$pred, melhor_threshold2) %>%
+  mutate(Modelo = "Modelo_6_var")
+
+# Unindo os resultados
+metricas_ajustadas <- bind_rows(metricas_m1, metricas_m2)
+
+
+
+tabela_comparativa_final <- gerar_tabela_estatistica(metricas_ajustadas)
+
+View(tabela_comparativa_final)
+plot_comparativo_metricas(tabela_comparativa_final)
+
+## É possível observar que o desempenho dos dois modelos é similar, mas o modelo com 6 variáveis é
+## mais estável (intervalos de confiança de menor amplitude)
+## Pelo princípio da parcimônia, optamos pelo modelo reduzido
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+summary(modelo_sem)
+# AIC: 122.43
 
 
 
 # Criando árove usando as variáveis do modelo reduzido
 modelo_arvore <- rpart(condition ~ sex + cp + oldpeak + slope + ca + thal, 
-                       data = treino, 
+                       data = treino_sem_outliers, 
                        method = "class") 
 
 # Árvore criada
@@ -155,21 +196,13 @@ rpart.plot(modelo_arvore,
 
 summary(modelo_reduzido_)
 
-# Extrai Odds Ratio e Intervalo de Confiança de 95%
 View(exp(cbind(OR = coef(modelo_reduzido_), confint(modelo_reduzido_))))
-
-table(treino$ca, treino$condition)
-
-
-cat("--- PERFORMANCE GLM REDUZIDO ---\n")
-print(confusionMatrix(pred_glm, teste$condition, positive = "Doente")$overall["Accuracy"])
-
-cat("\n--- PERFORMANCE GLM INTERAÇÃO ---\n")
-print(confusionMatrix(pred_int, teste$condition, positive = "Doente")$overall["Accuracy"])
-
-cat("\n--- PERFORMANCE ÁRVORE ---\n")
-print(confusionMatrix(pred_arvore, teste$condition, positive = "Doente")$overall["Accuracy"])
+# O intervalo de confinança de algumas variáveis indicadoras está muito grande
+# Principalmente as variáveis indicadoras com pouca representatividade no dataset;
 
 
-importancia <- varImp(modelo2, scale = FALSE)
-plot(importancia, main = "Importância das Variáveis - Modelo Reduzido")
+
+
+
+
+
