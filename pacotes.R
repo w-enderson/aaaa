5b226ad72885abf2ecd8a046153ee43484bbb5b1
Wenderson Santos <125162176+w-enderson@users.noreply.github.com>
2026-01-27 10:20:32 -0300

    Add tidyverse to package installation and imports
    Added 'tidyverse' package to installation and included library imports for additional packages.

--- pacotes.R
+++ pacotes.R
@@ -1,3 +1,16 @@
 install.packages("pak")
 
-pak::pkg_install(c('caret', 'car', 'MLmetrics', 'rpart', 'rpart.plot'))
+pak::pkg_install(c('caret', 'car', 'MLmetrics', 'rpart', 'rpart.plot', 'tidyverse'))
+
+
+# Imports
+library(car)
+library(caret)
+library(pROC)
+library(dplyr)
+library(tidyr)
+library(rpart)
+library(rpart.plot)
+library(ggplot2)
+library(tidyverse)
+
