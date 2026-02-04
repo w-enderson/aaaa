c9ff9ec18343870f7749250b1904c0dc2f651b95
w-enderson <wendersjuv@gmail.com>
2026-02-04 17:03:07 -0300

    commit

--- installDeps.R
+++ installDeps.R
@@ -2,5 +2,6 @@ install.packages("pak")
 
 pak::pkg_install(c('caret', 'car', 'MLmetrics', 
                   'rpart', 'rpart.plot', 'tidyverse', 
-                  'here', 'pander'))
+                  'here', 'pander', 'precrec'))
 
+install.packages('rmarkdown')
