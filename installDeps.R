1f57726ea6d29ac5b348a11e4986762ca08ac9ce
w-enderson <wendersjuv@gmail.com>
2026-03-05 13:19:44 -0300

    atualizando resumo final

--- installDeps.R
+++ installDeps.R
@@ -1,5 +1,9 @@
+
 install.packages("pak")
 
+
+pak::cache_clean()
+pak::meta_clean()
 pak::pkg_install(c('caret', 'car', 'MLmetrics', 
                   'rpart', 'rpart.plot', 'tidyverse', 
                   'here', 'pander', 'precrec',
